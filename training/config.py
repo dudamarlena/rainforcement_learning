2fc6308d5ab1dc7354d08c82f5320823cb78e7df
maduda <marlena.duda@nokia.com>
2018-11-06 19:36:22 +0100

    Changed path to summary file

--- training/config.py
+++ training/config.py
@@ -6,8 +6,8 @@ import inspect
 CURRENT_DIR = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 PARENT_DIR = os.path.dirname(os.path.dirname(CURRENT_DIR))
 os.sys.path.insert(0, PARENT_DIR)
-MONITOR_PATH = 'C:\\Users\\Marlena\\Desktop\\inzynierka\\reinforcement learning\\Walker\\motor'
-SUMMARY_DIR = MONITOR_PATH
+MONITOR_PATH = 'C:\\Users\\Marlena\\Desktop\\inzynierka\\reinforcement learning\\training\\motor'
+SUMMARY_DIR = 'C:\\Users\\Marlena\\Desktop\\inzynierka\\reinforcement learning\\training\\summary'
 
 WALKER = "Walker2DBulletEnv-v0"
 HALFCHEETAH = "HalfCheetahBulletEnv-v0"
